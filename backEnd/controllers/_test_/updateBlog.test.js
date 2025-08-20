1c14776e4e159653fa0367707f945ade9e79b165
IKKYU <kunlaphat_@hotmail.com>
2025-08-20 11:44:24 +0700

    debug update.test

--- backEnd/controllers/_test_/updateBlog.test.js
+++ backEnd/controllers/_test_/updateBlog.test.js
@@ -1,38 +1,40 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import * as BlogModel from '../../models/blogs.model.js';
-import { updateBlog } from '../blog.controller.js';
+import { describe, it, expect } from "vitest";
+import * as BlogModel from "../../models/blogs.model.js";
+import { updateBlog } from "../blog.controller.js";
 
-test('updateBlog forwards errors when save fails', async () => {
-  const saveError = new Error('Save failed');
+describe("updateBlog", () => {
+  it("forwards errors when save fails", async () => {
+    const saveError = new Error("Save failed");
 
-  const fakeBlog = {
-    user: { equals: () => true },
-    save: async () => { throw saveError; }
-  };
+    const fakeBlog = {
+      user: { equals: () => true },
+      save: async () => {
+        throw saveError;
+      },
+    };
 
-  const originalFindById = BlogModel.default.findById;
-  BlogModel.default.findById = async () => fakeBlog;
+    const originalFindById = BlogModel.default.findById;
+    BlogModel.default.findById = async () => fakeBlog;
 
-  const req = {
-    params: { id: '1' },
-    body: {},
-    user: { _id: '1', role: 'user' }
-  };
+    const req = {
+      params: { id: "1" },
+      body: {},
+      user: { _id: "1", role: "user" },
+    };
 
-  const res = {
-    status: () => res,
-    json: () => {}
-  };
+    const res = {
+      status: () => res,
+      json: () => {},
+    };
 
-  let receivedError;
-  const next = (err) => {
-    receivedError = err;
-  };
+    let receivedError;
+    const next = (err) => {
+      receivedError = err;
+    };
+    await updateBlog(req, res, next);
 
-  await updateBlog(req, res, next);
+    expect(receivedError).toBe(saveError);
 
-  assert.strictEqual(receivedError, saveError);
-
-  BlogModel.default.findById = originalFindById;
+    BlogModel.default.findById = originalFindById;
+  });
 });
